01bf04f46a85b5eec605a208908a77539ac9d2c8
Steven McVey <smcvey1210@gmail.com>
2023-03-03 12:31:38 -0800

    commit tree 1737b18366d762338adfa9ee8e541e0fc2f1c73a

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,1 +1,46 @@
 # your code here
+
+class Stack
+
+  def initialize(limit = nil)
+    @limit = limit
+    @stack = []
+  end
+
+  def push(value)
+    if @limit == nil
+      return @stack.push(value)
+    else
+      raise Exception.new("Stack Overflow") unless @stack.size < @limit
+      @stack.push (value)
+    end
+  end
+
+  def pop
+    @stack.pop
+  end
+
+  def peek
+    @stack.last
+  end
+
+  def size
+    @stack.size
+  end
+
+  def empty?
+ 
+    @stack.size == 0
+  end
+
+  def full?
+    @stack.size == @limit
+  end
+
+  def search(value)
+    return -1 if !@stack.include?(value)
+    location = @stack.find_index(value) + 1
+    @stack.size - location
+  end
+
+end
